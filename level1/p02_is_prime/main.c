39d0662a0682610cc0c6853fb81c9ca0a114eacf
EdgedTL <tommylam7749@gmail.com>
2023-09-12 23:00:31 +0800

    Create a programme that checks prime numbers with sqrt(n) method.

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,23 @@
-#include <printf.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
 
 int main() {
-    printf("hello world!\n");
+    int a;
+    printf("Input an integer\n");
+    scanf("%d",&a);
+    if(a == 0 || a == 1){
+        printf("The number %d is not a prime",a);
+        return 0;
+    }
+    int sqrt_a = (int)floor(sqrt(a));
+    for (int i = 1; i<=sqrt_a;i++){
+        if (a%i ==0){
+            printf("The number %d is not a prime",a);
+            return 0;
+        }
+
+    }
+    printf("The number %d is a prime",a);
     return 0;
 }
