e23fd1fb674dd5ceb2f01b633e810e500b8f52b5
EdgedTL <tommylam7749@gmail.com>
2023-09-13 00:08:41 +0800

    Changes: Create a programme that prints out all prime numbers between 2 and 1000 Logic  :All non-prime numbers can be seperated into multiples of smaller prime numbers Method :initialise prime number 2 and process all odd numbers. Only factorise with previous primes saved in a list, and add any newly found primes to it

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,34 @@
-#include <printf.h>
+#include <stdio.h>
+#include <time.h>
+
+long long GetTime(){
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    long long time = (ts.tv_sec * 1000LL) + (ts.tv_nsec / 1000000LL);
+    return time;
+}
 
 int main() {
-    printf("hello world!\n");
-    return 0;
+    long long start_time = GetTime();
+    int prime[168] ;
+    prime[0]=2;
+    int previous_prime = 1;
+    printf("2, ");
+    for (int num = 3; num <= 1000; num+=2){
+
+        int factor = 0;
+        for (int i=0;i<previous_prime;i++){
+            if (num%prime[i]==0){
+            factor++;
+            }
+        }
+        if(factor == 0){
+            printf("%d, ",num);
+            prime[previous_prime] = num;
+            previous_prime ++;
+        }
+
+    }
+    long long end_time = GetTime();
+    printf("\n Time used: %lld milliseconds", (end_time - start_time));
 }
